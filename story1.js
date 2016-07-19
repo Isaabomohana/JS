3f0d1be78fe92d238a32c4ac6a90353228dd8761
mrisa <mrissaam@outlook.com>
2016-07-19 19:44:48 +0200

    gathering all th html to the index.html

--- story1.js
+++ story1.js
@@ -1,46 +1,46 @@
 var questionCursor = 0;
 
 var questions = [
-       {
-           title: "Wie geht es Ihnen",
-           options: ["gut Danke", "es geht", "gut"],
-           rightAnswer: 0,
-           image: ''
-       },
-       {
-           title: "bla bla ",
-           options: ["1", "2", "3"],
-           rightAnswer: 1,
-           image: ''
-       },
-       {
-         title: "bla bla bla ",
-           options: ["1", "2", "3"],
-           rightAnswer: 2,
-           image: ''
-       }
-   ];
+        {
+            title: "Wie geht es Ihnen",
+            options: ["gut Danke", "es geht", "gut"],
+            rightAnswer: 0,
+            image: ''
+        },
+        {
+            title: "bla bla ",
+            options: ["1", "2", "3"],
+            rightAnswer: 1,
+            image: ''
+        },
+        {
+          title: "bla bla bla ",
+            options: ["1", "2", "3"],
+            rightAnswer: 2,
+            image: ''
+        }
+    ];
 
 function isCorrentAnswer(options) {
-   var question = story.questions[questionCursor];
-   return question.options.indexOf(option) === question.rightAnswer;
+    var question = story.questions[questionCursor];
+    return question.options.indexOf(option) === question.rightAnswer;
 }
 function update () {
-   var question = questions[questionCursor]
-   document.getElementById('fragen').innerHTML = question.title ;
-   document.getElementById('ans1').innerHTML = question.options[0];
-   document.getElementById('ans2').innerHTML = question.options[1];
-   document.getElementById('ans3').innerHTML = question.options[2];
+    var question = questions[questionCursor]
+    document.getElementById('fragen').innerHTML = question.title ;
+    document.getElementById('ans1').innerHTML = question.options[0];
+    document.getElementById('ans2').innerHTML = question.options[1];
+    document.getElementById('ans3').innerHTML = question.options[2];
+}
+function answerChecked () {
+    for (var i=0; i = questions[i].options.length; i++){
+        if (questions[questionCursor].options[i].checked === questions[i].rightAnswer){
+            questionCursor +=1 ;
+         
+        } else{
+            console.log("false");
+        }
+
+    }
 }
 update();
-function nextQuestion () {
-  questionCursor = questionCursor + 1;
-  update();
-}
-
-/*question = { rightAnswer: 1 };
-checks = [false,true,true];
-isRight = checks[question.rightAnswer];*/
-
-
-
