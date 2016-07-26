27b89fb3b0b3de2d79323dd7db39d80e74e80576
mrisa <mrissaam@outlook.com>
2016-07-26 19:00:12 +0200

    loading story 1 is finished

--- story1.js
+++ story1.js
@@ -29,63 +29,66 @@
 }
 ];
 
-function isCorrentAnswer(options) {
-  var question = story.questions[questionCursor];
-  return question.options.indexOf(option) === question.rightAnswer;
-}
 function update () {
 
   var question = questions[questionCursor]
   document.getElementById('fragen').innerHTML = question.title ;
-  document.getElementById('ans1').innerHTML = question.options[0];
-  document.getElementById('ans2').innerHTML = question.options[1];
-  document.getElementById('ans3').innerHTML = question.options[2];
+  document.getElementById('ans0').innerHTML = question.options[0];
+  document.getElementById('ans1').innerHTML = question.options[1];
+  document.getElementById('ans2').innerHTML = question.options[2];
 }
-function answerChecked () {
-  for (var i=0; i = questions[i].options.length; i++){
+function answerChecked (evt) {
+  evt.preventDefault();
+    var rightAnswer = questions[questionCursor].rightAnswer;
+  if ($("[data-index="+rightAnswer+"]").prop("checked")){
+    console.log(rightAnswer);
+    questionCursor = questionCursor +1;
     console.log(questionCursor);
-    console.log(questions[questionCursor]);
-    
-    }
-  }
-  /*the html structur*/
-  function getCurentStoryEl () {
-    return document.getElementById('story-' + currentStory);
-  }
-
-  function makeContentItemActive (contentItem) {
-
-    $('.content-item').removeClass('active');
-    $('.content-item-' + contentItem).addClass('active');
-  }
-
-  function makeStoryActive (storyId) {
-    $('.story').removeClass('active');
-    $('#story-' + storyId).addClass('active');
-  }
-
-  function goToNextStory () {
-    currentStory++;
-    makeStoryActive(currentStory);
-  }
-  /*the html structur*/
-
-
-  function logIn (){
-    var user = $("#userName").val();
-    loginNames.push(user);
-    console.log(loginNames);
-    document.getElementById('currentUser').innerHTML = user;
-    $(".content").hide();
-  }
-
-  function setup (){
-    $('#storyLink').click(function() { makeContentItemActive('stories') })
-    $('#homePage').click(function() { makeContentItemActive('login') })
-    $('#aboutPage').click(function() { makeContentItemActive('about') })
     update();
+  } else {
+    console.log("wrong answer");
   }
-  setup();
+}
+/*the html structur*/
+function getCurentStoryEl () {
+  return document.getElementById('story-' + currentStory);
+}
+
+function makeContentItemActive (contentItem) {
+
+  $('.content-item').removeClass('active');
+  $('.content-item-' + contentItem).addClass('active');
+}
+
+function makeStoryActive (storyId) {
+  $('.story').removeClass('active');
+  $('#story-' + storyId).addClass('active');
+}
+
+function goToNextStory () {
+  currentStory++;
+  makeStoryActive(currentStory);
+}
+/*the html structur*/
+
+
+function logIn (){
+  var user = $("#userName").val();
+  loginNames.push(user);
+  console.log(loginNames);
+  document.getElementById('currentUser').innerHTML = user;
+  $(".content").hide();
+}
+
+function setup (){
+  $('#storyLink').click(function() { makeContentItemActive('stories') })
+  $('#homePage').click(function() { makeContentItemActive('login') })
+  $('#aboutPage').click(function() { makeContentItemActive('about') })
+  update();
+}
+
+$("#submit-answer").click(answerChecked)
+setup();
 
 
 
